26a16223cecb24bdf77b65ed623ed376fe6c97a4
Ry <57374659+cryptidgit@users.noreply.github.com>
2022-11-11 13:15:06 -0500

    Remove old code

--- public/js/foursite-wordpress-promotion-public.js
+++ public/js/foursite-wordpress-promotion-public.js
@@ -1,8 +1,5 @@
-(function ($) {
+window.addEventListener("DOMContentLoaded", () => {
   "use strict";
-
-  // iterate over this object's configurations and take appropriate action
-  console.log("client_side_triggered_config", client_side_triggered_config);
 
   /**
    * All of the code for your public-facing JavaScript source
@@ -32,117 +29,13 @@
    * practising this, we should strive to set a better example in our own work.
    */
 
-  /*
-  $.post(
-    "/wp-content/plugins/4site-wordpress-promotions/public/raw-code-data.php",
-    function (data) {
-      let jsonData;
-      try {
-        jsonData = JSON.parse(data);
-      } catch {
-        return false;
-      }
-
-      window.rawCodeTriggers = [];
-      window.lightboxArr = jsonData;
-
-      // if (jsonData["css"]) {
-      //   document.querySelector(
-      //     "head"
-      //   ).innerHTML += `<style type='text/css'>${jsonData.css}</style>`;
-      // }
-
-      // if (jsonData.javascript) {
-      //   document.querySelector("head").innerHTML += jsonData.javascript;
-      // }
-
-      // if (jsonData.html) {
-      //   document.querySelector("body").innerHTML += jsonData.html;
-      // }
-
-      for (let i = 0; i < jsonData.length; i++) {
-        const currentArr = jsonData[i];
-
-        if (currentArr.type == "raw_code") {
-          const cookie = currentArr.cookie;
-          const cookieExpiration = currentArr.cookie_hours;
-          const id = currentArr.id;
-          window.rawCodeTriggers[id] = false;
-          let trigger = currentArr.trigger;
-          const triggerType = getTriggerType(trigger);
-
-          if (!getCookie(cookie)) {
-            if (triggerType === false) {
-              trigger = 2000;
-            }
-            if (triggerType === "seconds") {
-              trigger = Number(trigger) * 1000;
-            }
-
-            if (triggerType === "seconds" || triggerType === false) {
-              window.setTimeout(() => {
-                addRawCode(currentArr);
-                if (cookieExpiration) {
-                  setCookie(cookie, cookieExpiration);
-                } else {
-                  setCookie(cookie);
-                }
-              }, trigger);
-              window.rawCodeTriggers[currentArr.id] = true;
-            }
-            if (triggerType === "exit") {
-              document.body.addEventListener("mouseout", (e) => {
-                if (e.clientY < 0 && !window.rawCodeTriggers[id]) {
-                  addRawCode(currentArr);
-                  if (cookieExpiration) {
-                    setCookie(cookie, cookieExpiration);
-                  } else {
-                    setCookie(cookie);
-                  }
-                  window.rawCodeTriggers[id] = true;
-                }
-              });
-            }
-            if (triggerType === "pixels") {
-              document.addEventListener(
-                "scroll",
-                function () {
-                  scrollTriggerPx(currentArr);
-                },
-                true
-              );
-            }
-            if (triggerType === "percent") {
-              document.addEventListener(
-                "scroll",
-                function () {
-                  scrollTriggerPercent(currentArr);
-                },
-                true
-              );
-            }
-
-            if (triggerType === "js") {
-              window.addEventListener("open-lightbox", openLightboxEvent);
-            }
-          } else if (triggerType == "js") {
-            window.addEventListener("open-lightbox", openLightboxEvent);
-          }
-        } else {
-          window.addEventListener("open-lightbox", openLightboxEvent);
-        }
-      }
-    }
-  );
-*/
-
   window.rawCodeTriggers = [];
 
   for (const property in client_side_triggered_config) {
     const type = client_side_triggered_config[property].promotion_type;
 
     if (type == "multistep_lightbox") {
-      addEventListener("open-lightbox", openLightboxEvent);
+      addEventListener("trigger-promotion", triggerPromotionEvent);
     } else if (type == "raw_code") {
       const cookie = client_side_triggered_config[property].cookie;
       const cookieExpiration =
@@ -206,10 +99,10 @@
         }
 
         if (triggerType === "js") {
-          window.addEventListener("open-lightbox", openLightboxEvent);
+          window.addEventListener("trigger-promotion", triggerPromotionEvent);
         }
       } else if (getCookie(cookie) && triggerType == "js") {
-        window.addEventListener("open-lightbox", openLightboxEvent);
+        window.addEventListener("trigger-promotion", triggerPromotionEvent);
       }
     }
   }
@@ -262,149 +155,118 @@
     }
   }
 
-  function addRawCode(array) {
-    if (array.css) {
+  function addRawCode(promotionConfig) {
+    const promotionClass = "promotion-" + promotionConfig.id;
+
+    if (promotionConfig.html) {
+      const htmlContainer = document.createElement("div");
+      htmlContainer.classList.add("promotion-html");
+      htmlContainer.classList.add("promotion-element");
+      htmlContainer.classList.add(promotionClass);
+      htmlContainer.innerHTML = promotionConfig.html;
+      document.body.appendChild(htmlContainer);
+    }
+
+    if (promotionConfig.css) {
       const newCSS = document.createElement("style");
-      const promotionClass = "promotion-" + array.id;
       newCSS.setAttribute("type", "text/css");
       newCSS.classList.add("promotion-css");
+      newCSS.classList.add("promotion-element");
       newCSS.classList.add(promotionClass);
-      newCSS.textContent = array.css;
-      document.head.appendChild(newCSS);
+      newCSS.textContent = promotionConfig.css;
+      document.body.appendChild(newCSS);
     }
-    if (array.js) {
+
+    if (promotionConfig.js) {
       // Remove line breaks between scripts
-      const scripts = array.js
-        .replace(/(\r\n|\n|\r)/gm, "")
-        .replace("><", ">|<")
-        .replace(">\x3C", ">|\x3C")
-        .split("|");
+      const jsContainer = document.createElement("div");
+      jsContainer.classList.add("promotion-js");
+      jsContainer.classList.add("promotion-element");
+      jsContainer.classList.add(promotionClass);
+      jsContainer.innerHTML = promotionConfig.js;
 
-      for (let i = 0; i < scripts.length; i++) {
-        const script = stringToElement(scripts[i], array);
-        document.head.appendChild(script);
-      }
-    }
-    if (array.html) {
-      // Remove line breaks between elements
-      const elements = array.html
-        .replace(/(\r\n|\n|\r)/gm, "")
-        .replace("><", ">|<")
-        .replace(">\x3C", ">|\x3C")
-        .split("|");
-
-      for (let i = 0; i < elements.length; i++) {
-        const element = stringToElement(elements[i], array);
-        document.body.appendChild(element);
-      }
+      document.body.appendChild(jsContainer);
+      Array.from(document.querySelector(".promotion-js").children).forEach(
+        (child) => {
+          eval(child.innerHTML);
+        }
+      );
     }
   }
 
-  function scrollTriggerPx(arr) {
-    const triggerValue = Number(arr.trigger.replace("px", ""));
-    if (window.scrollY >= triggerValue && !window.rawCodeTriggers[arr.id]) {
-      addRawCode(arr);
-      if (arr.cookie_hours) {
-        setCookie(arr.cookie, arr.cookie_hours);
+  function scrollTriggerPx(promotionConfig) {
+    const triggerValue = Number(promotionConfig.trigger.replace("px", ""));
+    if (
+      window.scrollY >= triggerValue &&
+      !window.rawCodeTriggers[promotionConfig.id]
+    ) {
+      addRawCode(promotionConfig);
+      if (promotionConfig.cookie_hours) {
+        setCookie(promotionConfig.cookie, promotionConfig.cookie_hours);
       } else {
         setCookie(cookie);
       }
-      window.rawCodeTriggers[arr.id] = true;
+      window.rawCodeTriggers[promotionConfig.id] = true;
     }
   }
-  function scrollTriggerPercent(arr) {
-    const triggerValue = Number(arr.trigger.replace("%", ""));
+  function scrollTriggerPercent(promotionConfig) {
+    const triggerValue = Number(promotionConfig.trigger.replace("%", ""));
     const clientHeight = document.documentElement.clientHeight;
     const scrollHeight = document.documentElement.scrollHeight - clientHeight;
     const target = (triggerValue / 100) * scrollHeight;
-    if (window.scrollY >= target && !window.rawCodeTriggers[arr.id]) {
-      addRawCode(arr);
-      if (arr.cookie_hours) {
-        setCookie(arr.cookie, arr.cookie_hours);
+    if (
+      window.scrollY >= target &&
+      !window.rawCodeTriggers[promotionConfig.id]
+    ) {
+      addRawCode(promotionConfig);
+      if (promotionConfig.cookie_hours) {
+        setCookie(promotionConfig.cookie, promotionConfig.cookie_hours);
       } else {
         setCookie(cookie);
       }
-      window.rawCodeTriggers[arr.id] = true;
+      window.rawCodeTriggers[promotionConfig.id] = true;
     }
   }
 
-  function openLightboxEvent(e) {
-    if (e.detail.lightbox_id) {
-      const lightbox = client_side_triggered_config[e.detail.lightbox_id];
-      lightbox.trigger = 0;
+  function triggerPromotionEvent(e) {
+    if (e.detail.promotion_id) {
+      const promotion = client_side_triggered_config[e.detail.promotion_id];
 
-      if (lightbox.promotion_type != "raw_code") {
-        window.DonationLightboxOptions = lightbox;
-        deleteCookie(lightbox.cookie_name);
+      if (!promotion) {
+        console.log("Invalid promotion");
+        return;
+      }
+
+      if (promotion.trigger != "js") {
+        console.log("Promotion not set to JS trigger");
+        return;
+      }
+
+      promotion.trigger = 0;
+
+      if (promotion.promotion_type != "raw_code") {
+        window.DonationLightboxOptions = promotion;
+        deleteCookie(promotion.cookie_name);
         removeOldPromotions();
         new DonationLightbox();
       } else {
         removeOldPromotions();
-        addRawCode(lightbox);
+        addRawCode(promotion);
       }
     }
-  }
-
-  function stringToElement(element, promotion) {
-    // Get text between tags
-    const promotionClass = "promotion-" + promotion.id;
-    let elementContent;
-    if (element.includes("/>")) {
-      elementContent = false;
-    } else {
-      elementContent = element.slice(
-        element.indexOf(">") + 1,
-        element.indexOf("</")
-      );
-    }
-
-    let elementNameEnd;
-    let hasAttributes = false;
-    if (
-      element.indexOf(" ") != -1 &&
-      element.indexOf(" ") > element.indexOf(">")
-    ) {
-      elementNameEnd = element.indexOf(">");
-    } else {
-      elementNameEnd = element.indexOf(" ");
-      hasAttributes = "true";
-    }
-
-    const elementName = element.slice(element.indexOf("<") + 1, elementNameEnd);
-
-    const newElement = document.createElement(elementName);
-    newElement.classList.add("promotion-element");
-    newElement.classList.add(promotionClass);
-
-    if (elementContent) {
-      newElement.textContent = elementContent;
-    }
-
-    if (hasAttributes) {
-      const attributes = element
-        .slice(element.indexOf(" ") + 1, element.indexOf(">"))
-        .split(" ");
-
-      for (let i = 0; i < attributes.length; i++) {
-        if (attributes[i].includes("=")) {
-          newElement.setAttribute(
-            attributes[i].split("=")[0],
-            attributes[i].split("=")[1].replace(/"/g, "")
-          );
-        }
-      }
-    }
-
-    return newElement;
   }
 
   function removeOldPromotions() {
     document
-      .querySelectorAll(
-        ".foursiteDonationLightbox,.promotion-element,.promotion-css"
-      )
+      .querySelectorAll(".foursiteDonationLightbox,.promotion-element")
       .forEach((promotion) => {
         promotion.remove();
       });
   }
-})(jQuery);
+
+  window.triggerPromotion = function (id) {
+    window.dispatchEvent(
+      new CustomEvent("trigger-promotion", { detail: { promotion_id: id } })
+    );
+  };
+});
