52026229b08856f905c060f4f4d8afcc960678e6
Fernando Santos <fern@ndo.io>
2023-05-24 12:17:25 -0300

    New Build

--- blocks/en-multistep/build/index.asset.php
+++ blocks/en-multistep/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => 'a5844117ded7a6f353ea');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => 'daf5a6f49109e33938a9');
